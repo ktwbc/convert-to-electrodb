c9438ecb1bf299d29f32e90eea80987f7e24d1bd
Kris Thom White <kris.white@eightounce.com>
2023-05-07 12:32:30 -0500

    fix: handle a wrapped decorator

--- src/lib/dynamo-types/get-paired-lines.ts
+++ src/lib/dynamo-types/get-paired-lines.ts
@@ -3,8 +3,23 @@ export const getPairedLines = (lines: string[], searchString: string) =>
   lines
     .map((line, index) => {
       if (line.includes(searchString)) {
-        return [line, lines[index + 1]];
+        if (!line.includes('})')) {
+          // appears that Decorator uses three lines instead of 1, so we have to grab them
+          return [line + lines[index + 1] + lines[index + 2], lines[index + 3]];
+        } else {
+          return [line, lines[index + 1]];
+        }
       }
       return null;
     })
     .filter((line) => line !== null);
+
+/**
+ *   @Decorator.FullGlobalSecondaryIndex('invite_email', 'course', { name: 'invited-index' })
+ *   public static readonly invited: Query.FullGlobalSecondaryIndex<DynamoTokens, string, string>;
+ *
+ *   @Decorator.FullGlobalSecondaryIndex('purchased_by_subscriber_id', 'id', {
+ *     name: 'purchased_by_subscriber_id-index'
+ *   })
+ *   public static readonly purchasedBySubscriber: Query.FullGlobalSecondaryIndex<DynamoTokens, string, string>;
+ */
